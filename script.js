ab576e6fbf576bc1c5ac05e2b6d91c8821c03e2a
Simone <simonesnehvide@gmail.com>
2026-09-14 09:00:48 +0200

    Tilføjede alle 8 boliger til siden

--- script.js
+++ script.js
@@ -3,5 +3,37 @@ fetch("data/destinations.json")
         return response.json();
     })
     .then(function (data) {
-        console.log(data.destinations);
+        // const destination = data.destinations[0];
+        // console.log(data.destinations[0]);
+
+        data.destinations.forEach(function (destination) {
+            const divElement = document.createElement("div");
+            divElement.classList.add("destination-card");
+
+            document.querySelector(".destinations")
+
+                .append(
+                    divElement
+                );
+
+            const imgElement = document.createElement("img");
+            imgElement.src = `img/${destination.image}`;
+            imgElement.classList.add("destination-image");
+
+            const headingElement = document.createElement("h2");
+            headingElement.textContent = destination.title;
+            headingElement.classList.add("destination-heading")
+
+            const destinationText = document.createElement("p");
+            destinationText.textContent = destination.destination;
+            destinationText.classList.add("destination-text")
+
+            divElement.append(
+                imgElement,
+                headingElement,
+                destinationText
+            );
+        });
+
+
     });
